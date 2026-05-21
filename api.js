4cec02d0fb2a72ce411646b0b5c312bc4aad8bac
D1D13T0 <109699@aluno.uricer.edu.br>
2026-05-20 21:04:02 -0300

    primeiro commit

---
+++ api.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Sou o Projeto de Node + Express');
+});
+
+app.listen(3000, () => {
+  console.log('modificacao teste at http://localhost:3000')
+})
